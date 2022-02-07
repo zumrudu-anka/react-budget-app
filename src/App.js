6e48300da048ff7dbfcece00c5b35b6872c06dde
Zümrüd-ü Anka <osmandurdag@hotmail.com>
2022-02-08 02:16:14 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Button,
+  Container,
+  Stack
+} from "react-bootstrap";
+import BudgetCard from "./components/BudgetCard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container className = "my-4">
+      <Stack direction="horizontal" gap="2" className="mb-4">
+        <h1 className="me-auto">Budgets</h1>
+        <Button variant="primary">Add Budget</Button>
+        <Button variant="outline-primary">Add Expense</Button>
+      </Stack>
+      <div style = {{
+        display : "grid",
+        gridTemplateColumns : "repeat(auto-fill, minmax(300px, 1fr))",
+        gap : "1rem",
+        alignItems : "flex-start"
+      }}>
+        <BudgetCard
+          name = "Entertainment"
+          gray
+          amount = {200}
+          max = {1000}
+        ></BudgetCard>
+      </div>
+    </Container>
+  )
 }
 
 export default App;
